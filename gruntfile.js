51e8323a9b7acaa4a148c388cf0c81d66f2b9a8e
ceshang <shangce919@hotmail.com>
2016-03-28 01:29:03 -0500

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -7,7 +7,9 @@ module.exports = function(grunt){
     //how long tasks take
     require('time-grunt')(grunt);
     //automatically load required grunt tasks
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt,{
+        useminPrepare:'grunt-usemin'
+    });
 
     grunt.initConfig({
         pkg:grunt.file.readJSON('package.json'),
@@ -23,12 +25,137 @@ module.exports = function(grunt){
                     'app/scripts/{,*/}*.js'
                 ]
             }
+        },
+        copy:{
+            dist:{
+                cwd:'app',
+                src:['**','!styles/**/*.css','!scripts/**/*.js'],
+                dest:'dist',
+                expand:true
+            },
+            fonts:{
+                files:[
+                    {
+                        expand:true,
+                        dot:true,
+                        cwd:'bower_components/bootstrap/dist',
+                        src:['fonts/*.*'],
+                        dest:'dist'
+                    },
+                    {
+                        expand:true,
+                        dot:true,
+                        cwd:'bower_components/font-awesome',
+                        src:['fonts/*.*'],
+                        dest:'dist'
+                    }
+                ]
+            }
+        },
+        clean:{
+            build:{
+                src:['dist/']
+            }
+        },
+        useminPrepare:{
+            html:'app/menu.html',
+            options:{
+                dest:'dist'
+            }
+        },
+        concat:{
+            options:{
+                separator:';'
+            },
+            dist:{}
+        },
+        uglify:{
+            dist:{}
+        },
+        cssmin:{
+            dist:{}
+        },
+        filerev:{
+            options:{
+                encoding:'utf8',
+                algorithm:'md5',
+                length:20
+            },
+            release:{
+                files:[{
+                    src:[
+                        'dist/scripts/*.js',
+                        'dist/styles/*.css',
+                    ]
+                }]
+            }
+        },
+        usemin:{
+            html:['dist/*.html'],
+            css:['dist/style/*.css'],
+            options:{
+                assetsDirs:['dist','dist/styles']
+            }
+        },
+        watch: {
+            copy: {
+                files: [ 'app/**', '!app/**/*.css', '!app/**/*.js'],
+                tasks: [ 'build' ]
+            },
+            scripts: {
+                files: ['app/scripts/app.js'],
+                tasks:[ 'build']
+            },
+            styles: {
+                files: ['app/styles/mystyles.css'],
+                tasks:['build']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base:{
+                        path: 'dist',
+                        options: {
+                            index: 'menu.html',
+                            maxAge: 300000
+                        }
+                    }
+                }
+            }
         }
     });
 
     grunt.registerTask('build',[
-        'jshint'
+        'clean',
+        'jshint',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy',
+        'filerev',
+        'usemin'
     ]);
+
+    grunt.registerTask('serve',['build','connect:dist','watch']);
 
     grunt.registerTask('default',['build']);
 };
